2645b5801a7dd831db8c955fd84645ab80d641b6
skyv26 <dev.aakashv@gmail.com>
2022-11-22 00:47:01 +0530

    Add class `item` in `item.rb` file

---
+++ src/item/item.rb
@@ -0,0 +1,32 @@
+require 'securerandom'
+require_relative '../utils/time_diff'
+
+class Item
+  attr_reader :genre, :author, :source, :label
+
+  def initialize(publish_date, archived: false)
+    @id = SecureRandom.hex(5)
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def can_be_archived?
+    TimeDiff.years(@publish_date) >= 10
+  end
+
+  def move_to_archive()
+    @archived = can_be_archived? if can_be_archived?
+  end
+
+  def genre=(genre)
+  end
+
+  def author=(author)
+  end
+
+  def source=(source)
+  end
+
+  def label=(label)
+  end
+end
