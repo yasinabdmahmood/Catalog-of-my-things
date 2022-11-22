ba170da4d30a80d4f0c29fe18ff9d826b10a4785
yasinabdmahmood <abd181103@gmail.com>
2022-11-22 16:44:28 +0300

    Fix rubocop errors

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -1,21 +1,21 @@
-require './src/music_album/music_album.rb'
+require './src/music_album/music_album'
 
 describe MusicAlbum do
   context 'test can_be_archived? method' do
     it 'should return true' do
-        music_album = MusicAlbum.new('20/11/2001',true)
-        result = music_album.can_be_archived?
-        expect(result).to eql true
+      music_album = MusicAlbum.new('20/11/2001', true)
+      result = music_album.can_be_archived?
+      expect(result).to eql true
     end
     it 'should return false' do
-        music_album = MusicAlbum.new('20/11/2018',true)
-        result = music_album.can_be_archived?
-        expect(result).to eql false
+      music_album = MusicAlbum.new('20/11/2018', true)
+      result = music_album.can_be_archived?
+      expect(result).to eql false
     end
     it 'should return false' do
-        music_album = MusicAlbum.new('20/11/2001',false)
-        result = music_album.can_be_archived?
-        expect(result).to eql false
+      music_album = MusicAlbum.new('20/11/2001', false)
+      result = music_album.can_be_archived?
+      expect(result).to eql false
     end
   end
 end
--- src/music_album/music_album.rb
+++ src/music_album/music_album.rb
@@ -1,11 +1,13 @@
-require './src/item/item.rb'
-class MusicAlbum <Item
-    attr_accessor :on_spotify
-    def initialize(published_date,on_spotify)
-        super(published_date)
-        @on_spotify = on_spotify
-    end
-    def can_be_archived?
-        super && @on_spotify
-    end
+require './src/item/item'
+class MusicAlbum < Item
+  attr_accessor :on_spotify
+
+  def initialize(published_date, on_spotify)
+    super(published_date)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    super && @on_spotify
+  end
 end
