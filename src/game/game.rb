6a40216481f7c62b5e492d9a132131ec96217f39
skyv26 <dev.aakashv@gmail.com>
2022-11-22 23:40:08 +0530

    Update the file using `TDD` method

--- src/game/game.rb
+++ src/game/game.rb
@@ -4,7 +4,7 @@ require 'date'
 
 class Game < Item
   attr_reader :multiplayer
-  
+
   def initialize(multiplayer, publish_date, last_played_at)
     super(publish_date)
     @multiplayer = multiplayer
@@ -12,6 +12,6 @@ class Game < Item
   end
 
   def can_be_archived?
-    super && (TimeDiff.years(@last_played_at) > 2)
+    super && (TimeDiff.new.years(@last_played_at) > 2)
   end
 end
