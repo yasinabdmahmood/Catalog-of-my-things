a2e3932b48380fc5a9b53acad9cc5b73372e2bfd
skyv26 <dev.aakashv@gmail.com>
2022-11-22 22:19:44 +0530

    Add `two-ways` relationships

--- src/item/item.rb
+++ src/item/item.rb
@@ -18,11 +18,23 @@ class Item
     @archived = can_be_archived? if can_be_archived?
   end
 
-  def genre=(genre); end
+  def genre=(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
+  end
 
-  def author=(author); end
+  def author=(author)
+    @author = author
+    author.items << self unless author.items.include?(self)
+  end
 
-  def source=(source); end
+  def source=(source)
+    @source = source
+    source.items << self unless source.items.include?(self)
+  end
 
-  def label=(label); end
+  def label=(label)
+    @label = label
+    label.items << self unless label.items.include?(self)
+  end
 end
