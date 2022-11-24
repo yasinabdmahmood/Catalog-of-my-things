089b2b774fa9c28c74b9ce9e5d9e3489989011da
skyv26 <dev.aakashv@gmail.com>
2022-11-25 01:36:06 +0530

    use `extend` method

--- utils/helper.rb
+++ utils/helper.rb
@@ -3,7 +3,9 @@ require_relative '../modules/store_data'
 require_relative '../modules/get_data'
 require_relative '../modules/user_input'
 
-include UserInput
-include Reconstruction
-include StoreData
-include GetData
+class Helper
+  extend UserInput
+  extend Reconstruction
+  extend StoreData
+  extend GetData
+end
