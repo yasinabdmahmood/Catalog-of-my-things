77952cadd4168a6b67a1533af9f7445ae8ffcc88
skyv26 <dev.aakashv@gmail.com>
2022-11-25 00:53:10 +0530

    Change `IO` method

--- modules/user_input.rb
+++ modules/user_input.rb
@@ -2,7 +2,7 @@ module UserInput
   def ask_for_user_inputs(*messages)
     user_inputs = []
     messages.each do |message|
-      puts message
+      print message
       user_inputs << gets.chomp
     end
     user_inputs
