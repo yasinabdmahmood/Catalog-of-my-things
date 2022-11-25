a427db680be0ebbc52ddbbe2a7a925083b4787d2
skyv26 <dev.aakashv@gmail.com>
2022-11-25 20:31:06 +0530

    complete `add_new_game` functionality

--- operations/add_new_game.rb
+++ operations/add_new_game.rb
@@ -6,16 +6,19 @@ require_relative '../utils/msg'
 def add_new_game
   title,
   pub_date,
+  color,
   last_played_at,
   multiplayer = Helper.ask_for_user_inputs(
     'Enter game\'s name : ',
-    'Enter game launch date : ',
-    'When was the last time you played (YYYY-MM-DD) : ',
+    'Enter game launch date [YYYY-MM-DD] : ',
+    'Enter the game\'s color : ',
+    'When was the last time you played [YYYY-MM-DD] : ',
     'Is it multiplayer game ? (y/n) : '
   )
   multiplayer = multiplayer.downcase == 'y'
   game = Game.new(multiplayer, pub_date, last_played_at)
-  label = Label.new(title, 'GAME')
+  label = Label.new(title, color)
   label.add_item(game)
+  Helper.store_game(game)
   [game, label]
 end
