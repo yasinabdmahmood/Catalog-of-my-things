69b7efc70f1e50209dbf043e3870e3cc27f379ca
skyv26 <dev.aakashv@gmail.com>
2022-11-25 20:27:21 +0530

    complete `add_new_album` functionality

--- operations/add_new_album.rb
+++ operations/add_new_album.rb
@@ -7,18 +7,21 @@ require_relative '../utils/msg'
 def add_new_album
   title,
   genre_name,
+  color,
   pub_date,
   on_spotify = Helper.ask_for_user_inputs(
     'Enter music\'s name : ',
     'Enter genre\'s name : ',
-    'Enter music launch date : ',
+    'Enter color name for genre : ',
+    'Enter music launch date [YYYY-MM-DD]: ',
     'Is it available on spotify ? (y/n) : '
   )
   on_spotify = on_spotify.downcase == 'y'
   music_album = MusicAlbum.new(pub_date, on_spotify)
-  label = Label.new(title, 'MUSIC')
+  label = Label.new(title, color)
   genre = Genre.new(genre_name)
   label.add_item(music_album)
   genre.add_item(music_album)
+  Helper.store_music_album(music_album)
   [music_album, label, genre]
 end
