857c3512aee7ca9e363ff94df5fc791785162dff
skyv26 <dev.aakashv@gmail.com>
2022-11-25 22:37:04 +0530

    change the `keys` name

--- modules/store_data.rb
+++ modules/store_data.rb
@@ -11,35 +11,37 @@ module StoreData
 
   def store_book(book)
     hashed_book = {
+      'id' => book.id,
+      'title' => book.label.title,
       'publisher' => book.publisher,
       'publish_date' => book.publish_date,
-      'cover_state' => book.cover_state,
-      'title' => book.label.title,
       'color' => book.label.color,
-      'first_name' => book.author.first_name,
-      'last_name' => book.author.last_name
+      'author_name' => "#{book.author.first_name} #{book.author.last_name}",
+      'cover_state' => book.cover_state
     }
     store('data/books.json', hashed_book)
   end
 
   def store_music_album(music_album)
     hashed_music_album = {
-      'on_spotify' => music_album.on_spotify,
+      'id' => music.id,
+      'music_title' => music_album.label.title,
+      'genre_name' => music_album.genre.name,
       'publish_date' => music_album.publish_date,
-      'title' => music_album.label.title,
-      'color' => music_album.label.color,
-      'name' => music_album.genre.name
+      'label_color' => music_album.label.color,
+      'on_spotify' => music_album.on_spotify
     }
     store('data/music_albums.json', hashed_music_album)
   end
 
   def store_game(game)
     hashed_game = {
+      'id' => game.id,
+      'game_title' => game.label.title,
+      'game_color' => game.label.color,
       'multiplayer' => game.multiplayer,
-      'publish_date' => game.publish_date,
       'last_played_at' => game.last_played_at,
-      'title' => game.label.title,
-      'color' => game.label.color
+      'publish_date' => game.publish_date
     }
     store('data/games.json', hashed_game)
   end
