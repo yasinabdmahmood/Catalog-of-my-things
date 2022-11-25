46ed06fa2d7a4e5638336fec3b2a79fba6a22a1d
skyv26 <dev.aakashv@gmail.com>
2022-11-25 20:22:32 +0530

    complete `add_new_book` functionality

--- operations/add_new_book.rb
+++ operations/add_new_book.rb
@@ -8,18 +8,21 @@ def add_new_book
   title,
   author_name,
   pub_name,
+  color,
   pub_date,
   book_status = Helper.ask_for_user_inputs(
     'Enter book\'s name : ',
     'Enter author\'s name : ',
     'Enter publisher\'s name : ',
-    'Enter publication date : ',
+    'Enter book\'s color : ',
+    'Enter publication date [YYYY-MM-DD] : ',
     'How is book\'s condition (good/bad) : '
   )
   book = Book.new(pub_name, pub_date, book_status)
-  label = Label.new(title, 'BOOK')
+  label = Label.new(title, color)
   label.add_item(book)
   author = Author.new(author_name.split[0], author_name.split[1])
   author.add_item(book)
+  Helper.store_book(book)
   [book, label, author]
 end
