b6e080ec2d4fc9bfc328df9a60758d6ede4a1b88
skyv26 <dev.aakashv@gmail.com>
2022-11-25 23:36:49 +0530

    Add more `helper`

--- utils/load_data.rb
+++ utils/load_data.rb
@@ -4,5 +4,8 @@ def load_data
   all_books = Helper.retrieve_books
   all_games = Helper.retrieve_games
   all_music = Helper.retrieve_music_album
-  [all_books, all_games, all_music]
+  all_authors = Helper.retrieve_authors
+  all_labels = Helper.retrieve_labels
+  all_genres = Helper.retrieve_genres
+  [all_books, all_games, all_music, all_authors, all_labels, all_genres]
 end
